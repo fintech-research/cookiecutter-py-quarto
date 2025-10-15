b992bd4e66ea2de3e7f0cd8a24d07ea6c8e44465
Vincent Grégoire <vincent.gregoire@gmail.com>
2025-10-15 15:15:07 -0400

    Fixed issues with devcontainer building

--- {{cookiecutter.project_name}}/.devcontainer/postCreateCommand.sh
+++ {{cookiecutter.project_name}}/.devcontainer/postCreateCommand.sh
@@ -6,5 +6,8 @@ curl -LsSf https://astral.sh/uv/install.sh | sh
 # Install Dependencies
 uv sync
 
+# Initializes Git is not already done
+[ -d .git ] || git init -b main
+
 # Install pre-commit hooks
 uv run pre-commit install --install-hooks
